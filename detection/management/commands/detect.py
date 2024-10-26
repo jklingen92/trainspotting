a028634547e6453893805f1192634711e6e39e50
Jake Klingensmith <jklingen@pop-os.localdomain>
2024-10-26 16:27:30 -0400

    Defer video processing

--- detection/management/commands/detect.py
+++ detection/management/commands/detect.py
@@ -5,7 +5,6 @@ import numpy as np
 from json import load
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from numpy import Infinity
 
 from detection.utils import ImageInterface, Video, Clip, milli2timestamp, FFMPEG_BASE
 
@@ -27,102 +26,115 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         dest = self.initialize_dest(options)
-        video_lists, (x1, y1), (x2, y2) = self.initialize_videos(options["videos"])
+        videos, (x1, y1), (x2, y2) = self.initialize_videos(options["videos"])
         self.x1 = x1
         self.x2 = x2
         self.y1 = y1
         self.y2 = y2
 
-        for list_num, video_list in enumerate(video_lists):
-            if len(video_lists) > 1:
-                self.stdout.write(f"Processing list {list_num} video lists")
-
-            unfinished = None
-            for i, video in enumerate(video_list):
-                self.stdout.write(f"Processing video {i + 1} of {len(video_list)}: {video.path}")
-                tail_frames = video.fps * options['buffer']
-                minlength = options['buffer'] + options['minlength']
-                clips = []
-
-                if unfinished is None:
-                    frame0 = None
-                    clip = None
-                else:
-                    frame0, clip = self.seek_matching_frame(video, unfinished.frame)
-                    clip.merge_path = unfinished.path
+        unfinished = None 
+        n_videos = len(options['videos'])  
+        while True:
+            try:
+                gap, video = next(videos)
+            except StopIteration:
+                if unfinished is not None:
+                    self.stdout.write(f"  Unable to finish clip: {unfinished.path}")
+                self.stdout.write(f"Processed {n_videos}. Clips deposited in {dest}")
+                break
+            
+            if gap:
+                if unfinished:
+                    self.stdout.write(f"  Found gap, unable to finish clip: {unfinished.path}")
                     unfinished = None
-                while True:
-                    success, frame1 = video.cap.read()
+                else:
+                    self.stdout.write(f"  Found gap, no clips compromised.")
+            
+            self.stdout.write(f"Processing video {i + 1} of {n_videos}: {video.path}")
+            tail_frames = video.fps * options['buffer']
+            minlength = options['buffer'] + options['minlength']
+            clips = []
 
-                    # Handle end of video
-                    if not success:
-                        if clip is not None:
-                            unfinished = self.Unfinished(frame0)
-                            clips.append(clip)
-                        break
-                    
-                    # Handle first image
-                    if frame0 is None:
-                        frame0 = self.process_frame(frame1)
-                        continue
+            if unfinished is None:
+                frame0 = None
+                clip = None
+            else:
+                self.stdout.write(f"  Found unfinished clip, seeking matching frame...")
+                frame0, clip = self.seek_matching_frame(video, unfinished.frame)
+                clip.merge_path = unfinished.path
+                unfinished = None
+            while True:
+                success, frame1 = video.cap.read()
 
-                    frame1 = self.process_frame(frame1)
-                    delta = self.compare_frames(frame0, frame1)
-                    
-                    # Output
+                # Handle end of video
+                if not success:
                     if clip is not None:
-                        status_str = f"Capturing: {milli2timestamp(video.pos_milli - clip.start)} | Stills: {clip.stills:4d}"
-                    else:
-                        status_str = "-" * 30
-                    self.stdout.write(f"Progress: {milli2timestamp(video.pos_milli):8} | {delta:06.3f} | {status_str}", ending="\r")
-                    
-                    # Handle movement between frames
-                    if delta > options["upper"]:
-                        if clip is not None:
-                            clip.stills = 0
-                        else:
-                            clip = Clip(video.pos_milli)
-                            clip.buff_start(options['buffer'])
-                    # Handle a still image
-                    elif delta < options["lower"] and clip is not None:
-                        clip.stills += 1
-                        if clip.stills >= tail_frames:
-                            clip.stills = 0
-                            clip.end = video.pos_milli
-                            if (unfinished is not None and unfinished.defer) or clip.duration >= minlength * 1000:
-                                clips.append(clip)
-                            clip = None
-
-                    frame0 = frame1
+                        unfinished = self.Unfinished(frame0)
+                        clips.append(clip)
+                    break
                 
-                # Use FFMPEG to write the clips to a file
-                self.stdout.write()
-                i = 0
-                for clip in clips:
-                    outfile = os.path.join(dest, clip.dest(video))
+                # Handle first image
+                if frame0 is None:
+                    frame0 = self.process_frame(frame1)
+                    continue
 
-                    self.stdout.write(f"  Clipping {i + 1} of {len(clips)}: {milli2timestamp(clip.start)} - {milli2timestamp(clip.end) if clip.end else 'End of video'}")
-                    if not options["fake"]:
-                        end_str = ""
-                        if clip.end is not None:
-                            end_str = f"-to '{clip.end}ms'"
-                        cmd = f"{FFMPEG_BASE} -ss '{clip.start}ms' {end_str} -i {video.path} -enc_time_base:v 1:24 {outfile}"
-                        print(cmd)
-                        os.system(cmd)
-                        
-                        if clip.merge_path and not options['nomerge']:
-                            # This block merges the first clip of the current video with the previous unfinished clip
-                            mergefile = os.path.join(dest, 'merge.mp4')
-                            os.system(f'echo "file {clip.merge_path}" >> merge.txt')
-                            os.system(f'echo "file {outfile}" >> merge.txt')
-                            os.system(f"{FFMPEG_BASE} -f concat -safe 0 -i merge.txt -c copy {mergefile}")
-                            os.system(f"mv {mergefile} {clip.merge_path}")
-                            os.system(f"rm -f merge.txt {outfile}")
+                frame1 = self.process_frame(frame1)
+                delta = self.compare_frames(frame0, frame1)
+                
+                # Output
+                if clip is not None:
+                    status_str = f"Capturing: {milli2timestamp(video.pos_milli - clip.start)} | Stills: {clip.stills:4d}"
+                else:
+                    status_str = "-" * 30
+                self.stdout.write(f"Progress: {milli2timestamp(video.pos_milli):8} | {delta:06.3f} | {status_str}", ending="\r")
+                
+                # Handle movement between frames
+                if delta > options["upper"]:
+                    if clip is not None:
+                        clip.stills = 0
+                    else:
+                        clip = Clip(video.pos_milli)
+                        clip.buff_start(options['buffer'])
+                # Handle a still image
+                elif delta < options["lower"] and clip is not None:
+                    clip.stills += 1
+                    if clip.stills >= tail_frames:
+                        clip.stills = 0
+                        clip.end = video.pos_milli
+                        if (unfinished is not None and unfinished.defer) or clip.duration >= minlength * 1000:
+                            clips.append(clip)
+                        clip = None
+
+                frame0 = frame1
+            
+            # Use FFMPEG to write the clips to a file
+            self.stdout.write()
+            i = 0
+            for clip in clips:
+                outfile = os.path.join(dest, clip.dest(video))
+
+                self.stdout.write(f"  Clipping {i + 1} of {len(clips)}: {milli2timestamp(clip.start)} - {milli2timestamp(clip.end) if clip.end else 'End of video'}")
+                if not options["fake"]:
+                    end_str = ""
+                    if clip.end is not None:
+                        end_str = f"-to '{clip.end}ms'"
+                    cmd = f"{FFMPEG_BASE} -ss '{clip.start}ms' {end_str} -i {video.path} -enc_time_base:v 1:24 {outfile}"
+                    self.stdout.write(cmd)
+                    os.system(cmd)
                     
-                # Check the last clip to see if it was truncated
-                if unfinished is not None and unfinished.defer:
-                    unfinished.path = outfile
-                    unfinished.defer = False
+                    if clip.merge_path and not options['nomerge']:
+                        # This block merges the first clip of the current video with the previous unfinished clip
+                        mergefile = os.path.join(dest, 'merge.mp4')
+                        os.system(f'echo "file {clip.merge_path}" >> merge.txt')
+                        os.system(f'echo "file {outfile}" >> merge.txt')
+                        os.system(f"{FFMPEG_BASE} -f concat -safe 0 -i merge.txt -c copy {mergefile}")
+                        os.system(f"mv {mergefile} {clip.merge_path}")
+                        os.system(f"rm -f merge.txt {outfile}")
+                
+            # Check the last clip to see if it was truncated
+            if unfinished is not None and unfinished.defer:
+                unfinished.path = outfile
+                unfinished.defer = False
 
     def initialize_dest(self, options):
         """Determine destination folder and create if necessary."""
@@ -138,29 +150,29 @@ class Command(BaseCommand):
 
         return dest
 
-    def initialize_videos(self, videos):
+    def initialize_videos(self, video_paths):
         """Sort videos by timestamp, check for gaps, and get a bounding box to watch for motion."""
-        videos = [Video(path) for path in videos]
-        videos.sort(key=lambda v: v.start)
+        self.stdout.write("Processing videos initially...")
+        video_paths.sort(key=lambda p: os.path.getctime(p))
 
-        success, img = videos[0].cap.read()
+        video0 = Video(video_paths[0])
+        success, img = video0.cap.read()
         if success:
             interface = ImageInterface(img)
             (x1, y1), (x2, y2) = interface.get_bounding_box()
         else:
-            raise Exception(f"{videos[0]} has no frames!")
-        videos[0].release()
+            raise Exception(f"{video0} has no frames!")
+        video0.release()
 
-        video_lists = [[videos[0]]]
-        for i in range(1, len(videos)):
-            prev = videos[i - 1]
-            curr = videos[i]
-            if prev.end >= curr.start:
-                video_lists[-1].append(curr)
-            else:
-                video_lists.append([curr])
+        def video_generator(video_paths):
+            last_video_end = None
+            for video_path in video_paths:
+                video = Video(video_path)
+                gap = last_video_end is None or last_video_end > video.start
+                yield gap, video
+                last_video_end = video.end
 
-        return video_lists, (x1, y1), (x2, y2)
+        return video_generator(video_paths), (x1, y1), (x2, y2)
 
     def process_frame(self, frame):
         frame = frame[self.y1:self.y2, self.x1:self.x2]
@@ -188,7 +200,7 @@ class Command(BaseCommand):
         os.system(f"rm -f offsets.json")
         
         video_offset = 0
-        smallest_offset = Infinity
+        smallest_offset = np.inf
         for stream in offsets["streams"]:
             if stream["codec_type"] == "video":
                 video_offset = float(stream["start_time"])
