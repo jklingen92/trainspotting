f5880d70298e89526497339855b782aa1bf824e2
Jake Klingensmith <jklingen@pop-os.localdomain>
2024-11-12 16:37:17 -0500

    Added logging

--- detection/management/commands/load_videos.py
+++ detection/management/commands/load_videos.py
@@ -14,9 +14,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         video_paths = options.pop("videos")
         videos = []
-        for video_path in video_paths:
+        num_videos = len(video_paths)
+        self.stdout.write(f"Processing {num_videos} videos...")
+        for i, video_path in enumerate(video_paths):
+            self.stdout.write(f"  Progress: {i + 1} of {num_videos} videos", ending="\r")
             video = Video(file=video_path, camera=self.params.camera)
             video.init(save=False)
             video.start = timezone.make_aware(datetime.strptime(video.filename, "VID_%Y%m%d_%H%M%S"))
             videos.append(video)
+        self.stdout.write(f"\nProcessed {num_videos} videos!")
         Video.objects.bulk_create(videos)
